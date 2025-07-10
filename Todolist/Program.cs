e0b077420f848ffcfd541063bd8973e467889a39
rajesh basina <rajeshlonewarrior@gmail.com>
2025-07-10 15:02:23 +0530

    Boolean operator for true and false

--- Todolist/Program.cs
+++ Todolist/Program.cs
@@ -5,4 +5,11 @@ Console.WriteLine("[A]dd all TODO's");
 Console.WriteLine("[R]emove all TODO's");
 Console.WriteLine("[E]xit");
 
+//Boolean Operators
+
+var userchoice = Console.ReadLine();
+bool isuserinput = userchoice == "ABC"; //BOOLEAN OPERATOR FOR TRUE STATEMENT
+bool isuserinput2 = userchoice != "abc"; //BOOLEAN OPERATOR FOR FALSE STATEMENT
+
 Console.ReadKey();
+
