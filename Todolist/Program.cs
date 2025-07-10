587f008687574b5571bf4b4c4447577eedaed574
rajesh basina <rajeshlonewarrior@gmail.com>
2025-07-10 14:49:05 +0530

    small piece of code for todolist

--- Todolist/Program.cs
+++ Todolist/Program.cs
@@ -1,10 +1,8 @@
-﻿Console.WriteLine("[E]xit");
+﻿Console.WriteLine("Hello world!");
 Console.WriteLine("What do you want to do?");
 Console.WriteLine("[S]ee all TODO's");
 Console.WriteLine("[A]dd all TODO's");
 Console.WriteLine("[R]emove all TODO's");
 Console.WriteLine("[E]xit");
 
-var userchoice = Console.ReadLine();
-bool isuserinputAbc = userchoice == "abc";
 Console.ReadKey();
