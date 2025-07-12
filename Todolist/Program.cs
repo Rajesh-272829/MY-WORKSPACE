a5e711e37b66290772b4fc77de716daafb37b7ea
rajesh basina <rajeshlonewarrior@gmail.com>
2025-07-12 09:44:43 +0530

    Conditional statments and cdoing question

--- Todolist/Program.cs
+++ Todolist/Program.cs
@@ -1,49 +1,41 @@
-﻿//Console.WriteLine("Hello World!");
-//Console.WriteLine("[S]ee all todo's");
-//Console.WriteLine("[A]dd a todo");
-//Console.WriteLine("[R]emove a todo");
-//Console.WriteLine("[E]xit");
+﻿//if else statement
 
-//Boolean operator
-//var userchoice = Console.ReadLine();
-//var userinput = userchoice == "ABC";
-//var userinput2=userchoice!="abc:";
+//var userChoice = Console.ReadLine();
 
+//if (userChoice.Length>10)
+//{
+//    Console.WriteLine("long answer");
+//}
 
+//else if (userChoice.Length<10)
+//{
+//    Console.WriteLine("Medium Answer");
+//}
 
-//Comparison Operator
-
-var number = 10;
-
-var islargerthan5 = number > 5;  //it should be true //greater than
-var ismallerthan5 = number < 5;  // it should be false //less than
-var isgreaterthanorequalto5 = number >= 5; // it should be true //greater than or equal to
-var islessthanorequalto5 = number <= 5; // it should be false //less than or equal to
-var isnumber10modulo3is1 = 10 % 3 == 1; //it is true because the remainder will be 1 and it is modulo operator
-var iseven = 10 % 2 == 0;
+//else
+//{
+//    Console.WriteLine("Short Answer");
+//}
 
 
-//AND, OR OPERATORS
 
-//var number=10;
-//var andoperator = number > 4 && number < 9
-//    ;
-//var orperator = number > 4 || number < 9 ||number <200;
-//var combooforand = number == 123 || (number % 2 == 0 && number < 20);
+
+//CODING QUESTION
+
+//int number = 0;
+//String result;
+
+//if (number<0)
+//{
+//    result = "negative";
+//}
+//else if(number==0)
+//{
+//    result = "positive";
+//}
+//else
+//{
+//    result = "negative";
+//}
+
 //Console.ReadKey();
-
-
-
-//CODING EXERCISE FOR AND, OR OPERATOR
-
-//var a = 5;
-//var b = 12;
-
-//var isthesumofvalueaandbgreaterthan17 = a + b >= 17 || a > 17;
-//Console.ReadKey();
-
-
-
-
-
-
